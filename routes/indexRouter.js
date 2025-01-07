2cea2da9291656b2abacd9f6220d1269d74e949f
samjmccormick <samjmccormick@gmail.com>
2025-01-06 16:40:05 -0800

    added unique id and link to individual message page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,11 +6,13 @@ const indexRouter = Router();
 // sample messages
 const messages = [
   {
+    id: Math.random().toString(16).slice(2),
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: Math.random().toString(16).slice(2),
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -28,8 +30,14 @@ indexRouter.get("/new", (req, res) => {
   res.render("form", { username: username });
 });
 
+indexRouter.get("/message/:id", (req, res) => {
+  const message = messages.find((message) => message.id === req.params.id);
+  res.render("messageDetail", { message: message, username: username });
+});
+
 indexRouter.post("/new", (req, res) => {
   messages.push({
+    id: Math.random().toString(16).slice(2),
     text: req.body.text,
     user: req.body.user,
     added: new Date(),
